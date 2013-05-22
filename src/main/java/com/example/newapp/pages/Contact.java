384b1d666b536a852e8d4372c3c9d990a4775722
xy6er <xy6er@yandex-team.ru>
2013-05-22 08:13:32 +0400

    final !!!

--- src/main/java/com/example/newapp/pages/Contact.java
+++ src/main/java/com/example/newapp/pages/Contact.java
@@ -1,4 +1,36 @@
 package com.example.newapp.pages;
 
+import org.apache.tapestry5.PersistenceConstants;
+import org.apache.tapestry5.annotations.OnEvent;
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.hibernate.annotations.CommitAfter;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.hibernate.Session;
+
+import java.util.Date;
+
 public class Contact {
+    @Inject
+    private Session session;
+
+    @Persist(PersistenceConstants.FLASH)
+    @Property
+    private String message;
+
+    @Property
+    private String fio;
+    @Property
+    private String email;
+    @Property
+    private String text;
+
+
+    @CommitAfter
+    @OnEvent(component = "addButton", value = "selected")
+    void onSuccessForm()
+    {
+        message = "Сообщение успешно отправлено. Большое спасибо, что написали нам!";
+    }
+
 }
